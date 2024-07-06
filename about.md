3d796c8e44ac69e457c8ad74d672782ab41a3ed4
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-07-06 00:16:05 -0400

    Initial commit

---
+++ about.md
@@ -0,0 +1,9 @@
+# RAM Limiter
+
+This mod forces levels to stay within a RAM limit. 
+
+Counterintuitive you'd think since GD just became 64 bit on Windows, but some levels can exponentially use up your ram, and you don't want your whole PC crashing due to a GD level. 
+
+With this mod, you can set the limit from 128 MiB to any higher value. 
+
+For example: 1 GiB is 1024 MiB, and by defualt, this is set to a limit of 4 GiB (4096 MiB). 
---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include "psapi.h"
+
+using namespace geode::prelude;
+
+class $modify(MyPlayLayer, PlayLayer) {
+
+	struct Fields {
+		unsigned int m_initialRam = 0;
+	};
+
+	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
+			return false;
+		}
+
+     	MEMORYSTATUSEX memInfo;
+		memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+		GlobalMemoryStatusEx(&memInfo);
+
+		PROCESS_MEMORY_COUNTERS_EX pmc;
+		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+        SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
+
+        m_fields->m_initialRam = pmc.WorkingSetSize/1048576;
+
+        schedule(schedule_selector(MyPlayLayer::checkRam));
+
+		return true;
+	}
+
+	void checkRam(float dt){
+
+		if(Mod::get()->getSettingValue<bool>("enable-ram-limit")){
+			MEMORYSTATUSEX memInfo;
+			memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+			GlobalMemoryStatusEx(&memInfo);
+
+			PROCESS_MEMORY_COUNTERS_EX pmc;
+			GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+			SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
+
+			unsigned int usedMemory = pmc.WorkingSetSize/1048576;
+			unsigned int levelMemory = usedMemory - m_fields->m_initialRam;
+
+			if(levelMemory > Mod::get()->getSettingValue<int64_t>("ram-limit")){
+				onQuit();
+			}
+		}
+	}
+};
