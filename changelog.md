3e092392c732179fd27d507ab88ec1ff7340b41c
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2025-01-25 18:03:40 -0500

    code cleanup

--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
+# 1.0.5
+- Clean up code
+
 # 1.0.2
 - Fixed a bug where sometimes a kick will happen for no reason
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,47 +4,50 @@
 
 using namespace geode::prelude;
 
+int ramLimit = 0;
+
 class $modify(MyPlayLayer, PlayLayer) {
 
-	struct Fields {
-		int m_initialRam = 0;
-	};
+    struct Fields {
+        int m_initialRam = 0;
+    };
 
-	bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-		if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
-			return false;
-		}
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
+            return false;
+        }
+        if (Mod::get()->getSettingValue<bool>("enable-ram-limit")){
 
-     	MEMORYSTATUSEX memInfo;
-		memInfo.dwLength = sizeof(MEMORYSTATUSEX);
-		GlobalMemoryStatusEx(&memInfo);
+            ramLimit = Mod::get()->getSettingValue<int64_t>("ram-limit");
 
-		PROCESS_MEMORY_COUNTERS_EX pmc;
-		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+            MEMORYSTATUSEX memInfo;
+            memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+            GlobalMemoryStatusEx(&memInfo);
 
-        m_fields->m_initialRam = pmc.WorkingSetSize/1048576;
+            PROCESS_MEMORY_COUNTERS_EX pmc;
+            GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
 
-        schedule(schedule_selector(MyPlayLayer::checkRam));
+            m_fields->m_initialRam = pmc.WorkingSetSize/1048576;
+            
+            schedule(schedule_selector(MyPlayLayer::checkRam));
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	void checkRam(float dt){
+    void checkRam(float dt){
+        MEMORYSTATUSEX memInfo;
+        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&memInfo);
 
-		if(Mod::get()->getSettingValue<bool>("enable-ram-limit")){
-			MEMORYSTATUSEX memInfo;
-			memInfo.dwLength = sizeof(MEMORYSTATUSEX);
-			GlobalMemoryStatusEx(&memInfo);
+        PROCESS_MEMORY_COUNTERS_EX pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
 
-			PROCESS_MEMORY_COUNTERS_EX pmc;
-			GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+        int usedMemory = pmc.WorkingSetSize/1048576;
+        int levelMemory = usedMemory - m_fields->m_initialRam;
 
-			int usedMemory = pmc.WorkingSetSize/1048576;
-			int levelMemory = usedMemory - m_fields->m_initialRam;
-
-			if(levelMemory > Mod::get()->getSettingValue<int64_t>("ram-limit")){
-				onQuit();
-			}
-		}
-	}
+        if (levelMemory > ramLimit) {
+            onQuit();
+        }
+    }
 };
